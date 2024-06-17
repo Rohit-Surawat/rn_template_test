fd92fc99ec8ea25543fc01a06edf1b9210169849
Rohit Surawat <rajrohitsu01@gmail.com>
2024-06-17 20:11:26 +0400

    first commit

---
+++ template/ios/Pods/Target Support Files/Pods-ProjectForTemplate-ProjectForTemplateTests/Pods-ProjectForTemplate-ProjectForTemplateTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ProjectForTemplate_ProjectForTemplateTests : NSObject
+@end
+@implementation PodsDummy_Pods_ProjectForTemplate_ProjectForTemplateTests
+@end
